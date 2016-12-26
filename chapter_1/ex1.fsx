1e1984a0be5708cd4ced25a02d62846951c661f9
Alan Gou <alanjgou@gmail.com>
2016-12-26 19:18:01 +0800

    Started building a tree structure

---
+++ chapter_1/ex1.fsx
@@ -0,0 +1,19 @@
+type Key = string
+type Tree =
+    | Leaf
+    | Tree of Tree * Key * Tree
+
+let empty = Leaf
+
+let rec insert key tree =
+    match tree with
+    | Leaf ->
+        Tree(Leaf, key, Leaf)
+    | Tree(l, k, r) ->
+        if key < k
+            then Tree((insert key l), k, r)
+        else if key > k
+            then Tree(l, k, (insert key r))
+        else
+            Tree(l, key, r)
+
